8fecb1b0dde3e61e062d35155590847b9e8f700e
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-02-29 16:48:42 +0100

    A Project Management Application with Laravel

---
+++ app/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //
+    protected $fillable = [
+        'name',
+        'description',
+        'days',
+        'hours',
+        'company_id',
+        'user_id'
+    ];
+
+    public function company() {
+        return $this->belongsTo('App/Company');
+    }
+
+    public function users() {
+        return $this->belongsToMany('App/User');
+    }
+}
---
+++ database/migrations/2020_02_29_100053_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->integer('days')->unsigned()->nullable();
+            $table->integer('hours')->unsigned()->nullable();
+            $table->bigInteger('company_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
